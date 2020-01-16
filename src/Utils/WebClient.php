981e1564b3021b96b13937edee9331e83a592be7
ins7 <inser7@gmail.com>
2020-01-16 21:39:08 +0300

    cleanup

--- src/Utils/WebClient.php
+++ src/Utils/WebClient.php
@@ -13,35 +13,42 @@ class WebClient implements WebClientInterface
      * Numeric status code, 200: OK
      */
     const HTTP_OK = 200;
+    
     /**
      * Numeric status code, 201: Created
      */
     const HTTP_CREATED = 201;
+
     /**
      * Numeric status code, 500: Internal error
      */
     const HTTP_INTERNAL_ERROR = 500;
+
     /**
      * Numeric status code, 400: Bad Request
      */
     const HTTP_BAD_REQUEST = 400;
+
     /**
      * Numeric status code, 404: Not found
      */
     const HTTP_NOT_FOUND = 404;
+
     /**
      * Numeric status code, 304: Not modified
      */
     const HTTP_NOT_MODIFIED = 304;
+
+
     /**
      * Numeric status code, 301 Moved permanently
      */
     const HTTP_MOVED_PERM = 301;
+
     /**
      * Numeric status code, 302 Moved Temporarily
      */
     const HTTP_MOVED_TEMP = 302;
-
 
     /**
      * Is the response code a 200 OK?
@@ -62,6 +69,7 @@ class WebClient implements WebClientInterface
     {
         return (integer) $this->_response->getStatusCode();
     }
+
     /**
      * URL escaping
      *
